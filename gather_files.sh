45367daa2962d05df2aa97b6f4cdf7875351081b
Fabien Brulport <fabien.brulport@gmail.com>
2020-05-01 23:42:10 +0200

    Add 2 bash scripts to check if the git is outdated, and to copy the files in the git

---
+++ gather_files.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Oh-My-Zsh
+cp ~/.oh-my-zsh/custom/conda_venv_prompt.zsh oh-my-zsh
+cp ~/.oh-my-zsh/custom/color_patch.zsh oh-my-zsh
+cp ~/.oh-my-zsh/custom/themes/my-theme.zsh-theme oh-my-zsh
+
+# Vim
+cp ~/.vimrc vim
+
+# Tmux
+cp ~/.tmux.conf tmux
+
---
+++ is_outdated.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+function_check_diff () {
+    if [[ $(diff $1 $2) ]]; then
+        echo Difference found in $(basename $2)
+        diff $1 $2
+        echo 
+    fi
+}
+
+# Oh-My-Zsh
+function_check_diff ~/.oh-my-zsh/custom/conda_venv_prompt.zsh oh-my-zsh/conda_venv_prompt.zsh
+function_check_diff ~/.oh-my-zsh/custom/color_patch.zsh oh-my-zsh/color_patch.zsh
+function_check_diff ~/.oh-my-zsh/custom/themes/my-theme.zsh-theme oh-my-zsh/my-theme.zsh-theme
+
+# Vim
+function_check_diff ~/.vimrc vim/.vimrc
+
+# Tmux
+function_check_diff ~/.tmux.conf tmux/.tmux.conf
+
