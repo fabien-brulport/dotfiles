01b3f48fac17d414d4f18a5fdd7fd89d6184a87e
Fabien Brulport <fabien.brulport@gmail.com>
2020-05-01 23:53:38 +0200

    Handle light and dark themes automatically in zsh auto suggestions

--- oh-my-zsh/color_patch.zsh
+++ oh-my-zsh/color_patch.zsh
@@ -1,4 +1,11 @@
 # To put in ~/.oh-my-zsh/custom/
 
-# This is needed with solarized theme
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=007'
+# This is needed with solarized themes:
+# Set the foreground higtlighting accordingly to the current mac os mode (dark or light)
+# Note: need to source ~/.zshrc to change to colour on the open terminals
+if [ $(defaults read -g AppleInterfaceStyle) = 'Dark' ]
+then
+	ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=000'
+else
+	ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=007'
+fi
