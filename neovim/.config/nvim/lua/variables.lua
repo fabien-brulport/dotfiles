8f4bb9e9913f15b238dade4b16d41304ce45d4ae
Fabien Brulport <fabien.brulport@gmail.com>
2022-07-21 10:23:21 +0200

    remove hardcoded home

--- neovim/.config/nvim/lua/variables.lua
+++ neovim/.config/nvim/lua/variables.lua
@@ -1,2 +1,2 @@
-vim.g.python3_env = '/Users/fbrulport/.local/share/virtualenvs/neovimvenv'
-vim.g.python3_host_prog = '/Users/fbrulport/.local/share/virtualenvs/neovimvenv/bin/python'
+vim.g.python3_env = os.getenv( "HOME" ) .. '/.local/share/virtualenvs/neovimvenv'
+vim.g.python3_host_prog = os.getenv( "HOME" ) .. '/.local/share/virtualenvs/neovimvenv/bin/python'
