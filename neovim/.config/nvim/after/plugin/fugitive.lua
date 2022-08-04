b5245cad4ddd76e392aac41833d6cca95a991eb6
Fabien Brulport <fabien.brulport@gmail.com>
2022-08-04 11:00:59 +0200

    Some fugitive keymaps

--- neovim/.config/nvim/after/plugin/fugitive.lua
+++ neovim/.config/nvim/after/plugin/fugitive.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<leader>gg", "<cmd>vertical Git<cr>")
-vim.keymap.set("n", "<leader>gc", "<cmd>vertical Gclog<cr>")
+vim.keymap.set("n", "<leader>gl", "<cmd>vertical Gclog<cr>")
 vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiff<cr>")
+vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>")
+vim.keymap.set("n", "<leader>gp", function() require("toggleterm").exec("git push", 1, 12) end)
