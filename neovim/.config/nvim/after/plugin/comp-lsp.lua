de64f867593144bead09557040e0be7e5b358cf7
Fabien Brulport <fabien.brulport@gmail.com>
2022-10-26 10:31:42 +0200

    add rust_analyzer

--- neovim/.config/nvim/after/plugin/comp-lsp.lua
+++ neovim/.config/nvim/after/plugin/comp-lsp.lua
@@ -80,16 +80,22 @@ local function get_python_path(workspace)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require'lspconfig'.pyright.setup{
+local lsp_flags = {
+    debounce_text_changes = 150,
+  }
+require('lspconfig').pyright.setup{
   on_attach = on_attach,
   before_init = function(_, config)
     python = get_python_path(config.root_dir)
     vim.g.pythonLSPpath = python
     config.settings.python.pythonPath = python
   end,
-  flags = {
-    debounce_text_changes = 150,
-  },
+  flags = lsp_flags,
+  capabilities = capabilities,
+}
+require('lspconfig')['rust_analyzer'].setup{
+  on_attach = on_attach,
+  flags = lsp_flags,
   capabilities = capabilities,
 }
 
