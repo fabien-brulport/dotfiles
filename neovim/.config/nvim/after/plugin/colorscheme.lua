c9b00450e55ef6de79eec9f105a8450093acd5b9
Fabien Brulport <fabien.brulport@gmail.com>
2023-05-11 12:34:53 -0400

    better color for the term

--- neovim/.config/nvim/after/plugin/colorscheme.lua
+++ neovim/.config/nvim/after/plugin/colorscheme.lua
@@ -1,4 +1,11 @@
+-- setup must be called before loading the colorscheme
+-- Default options:
+require("gruvbox").setup({
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  invert_signs = false,
+  transparent_mode = true,
+})
 vim.opt.termguicolors = true
 vim.cmd("colorscheme gruvbox")
 -- To set the background transparent
-vim.api.nvim_set_hl(0, "Normal", {guibg = NONE, ctermbg = NONE})
+-- vim.api.nvim_set_hl(0, "Normal", {guibg = NONE, ctermbg = NONE})
--- neovim/.config/nvim/after/plugin/toggleterm.lua
+++ neovim/.config/nvim/after/plugin/toggleterm.lua
@@ -3,4 +3,15 @@ require("toggleterm").setup{
   direction = "float",
   insert_mappings = true,
   terminal_mappings = true,
+  -- shade_terminals = false,
+  highlights = {
+    -- highlights which map to a highlight group name and a table of it's values
+    -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+    Normal = {
+      link = 'WinBar'
+    },
+    NormalFloat = {
+      link = 'WinBar'
+    },
+  },
 }
