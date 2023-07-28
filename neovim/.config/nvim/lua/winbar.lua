b2727d78c31710e39733e5d8a54e74fa1f067d33
Fabien Brulport <fabien.brulport@gmail.com>
2023-07-28 11:10:39 +0200

    rework statusline

--- neovim/.config/nvim/lua/winbar.lua
+++ neovim/.config/nvim/lua/winbar.lua
@@ -1,18 +1,15 @@
-if vim.fn.has("nvim-0.8.0") == 1 then
-
-  local get_file_name = function()
-    local filename, extension = vim.fn.expand("%:t"), vim.fn.expand("%:e")
-    local icon, color = require('nvim-web-devicons').get_icon_color(filename, extension)
-    return '%m ' ..  icon .. ' %f' 
-  end
-
-  function win_bar()
-    return table.concat {
-      '%#Underlined#',
-      '%=',
-      get_file_name(),
-    }
-  end
-
-  vim.o.winbar = "%!luaeval('win_bar()')"
+local get_file_name = function()
+  local filename, extension = vim.fn.expand("%:t"), vim.fn.expand("%:e")
+  local icon, _ = require('nvim-web-devicons').get_icon_color(filename, extension)
+  return string.format('%%m %s %%f', icon)
 end
+
+function win_bar()
+  return table.concat {
+    '%#Underlined#',
+    '%=',
+    get_file_name(),
+  }
+end
+
+vim.o.winbar = "%!luaeval('win_bar()')"
