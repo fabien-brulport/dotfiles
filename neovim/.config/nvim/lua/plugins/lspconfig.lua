a67a45c854833faf927f0ec8d54d3b7b67432829
Fabien Brulport <fabien.brulport@gmail.com>
2023-10-10 11:19:22 +0200

    better keymaps and floating window for diagnostics

--- neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -120,10 +120,21 @@ return {
 
     -- Global mappings.
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+    vim.keymap.set('n', '<space>dd', vim.diagnostic.open_float)
+    vim.keymap.set('n', '<space>dp', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', '<space>dn', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<space>dq', vim.diagnostic.setqflist)
+    vim.diagnostic.config({
+      virtual_text = false,
+      float = {
+        focusable = false,
+        style = 'minimal',
+        border = 'none',
+        source = 'if_many',
+        header = '',
+        prefix = '',
+      },
+    })
 
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
