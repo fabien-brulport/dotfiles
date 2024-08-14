f23e538ce2f85d3afe874b8748cc3f37b190dbcf
Fabien Brulport <fabien.brulport@gmail.com>
2024-08-14 15:59:32 +0200

    feat(neovim): setup quicker.nvim

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -58,4 +58,37 @@ return {
       { "-", "<CMD>Oil<CR>", desc = "Open parent directory with Oil" },
     }
   },
+  {
+    'stevearc/quicker.nvim',
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    keys = {
+      {
+        "<leader>q",
+        function()
+          require("quicker").toggle()
+        end,
+        desc = "Toggle quickfix",
+      },
+    },
+    opts = {
+      keys = {
+        {
+          ">",
+          function()
+            require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+          end,
+          desc = "Expand quickfix context",
+        },
+        {
+          "<",
+          function()
+            require("quicker").collapse()
+          end,
+          desc = "Collapse quickfix context",
+        },
+      },
+    },
+  }
 }
